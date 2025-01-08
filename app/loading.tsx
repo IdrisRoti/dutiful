87f876019feb1eaeae518007bb747682be2a8a01
IdrisRoti <idrisomisakin@gmail.com>
2025-01-08 14:32:09 +0100

    feat: add simple loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='w-screen h-screen grid place-items-center text-2xl font-semibold uppercase'>Loading...</div>
+  )
+}
+
+export default Loading
